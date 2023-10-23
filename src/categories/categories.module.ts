77b56e103d96eda4e3dc1a84e2990b7f3c2d66e0
Kallew Machado <kallewcarlos@gmail.com>
2023-10-23 11:18:03 -0300

    feat: remove FirebaseService of providers

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,11 +3,10 @@ import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { CommonModule } from 'src/common/common.module';
 import { AuthModule } from 'src/auth/auth.module';
-import { FirebaseService } from 'src/auth/firebase/firebase.service';
 
 @Module({
   imports: [CommonModule, AuthModule],
   controllers: [CategoriesController],
-  providers: [CategoriesService, FirebaseService],
+  providers: [CategoriesService],
 })
 export class CategoriesModule {}
